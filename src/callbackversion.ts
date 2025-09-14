5ea447de3ffa418ab469210c9dc1820c47cbbf80
lindo-semetsi-hub <lindosemetsi@gmail.com>
2025-09-15 00:14:05 +0200

    commit current code

--- src/callbackversion.ts
+++ src/callbackversion.ts
@@ -1,1 +1,22 @@
-console.log("run callback version");
+import * as https from "http";
+
+function fetchJSON(url: string, callback: (err: any, data?: any) => void) {
+    https.get(url, (res) => {
+        let rawData = "";
+
+        res.on("data", (chunk) => {
+            rawData += chunk;
+        });
+
+        res.on("end", () => {
+            try {
+                const parsedData = JSON.parse(rawData);
+                callback(null, parsedData);
+            } catch (err) {
+                callback(err);
+            }
+        });
+    }).on("error", (err) => {
+        callback(err);
+        });
+    }
