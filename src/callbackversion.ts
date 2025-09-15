8fdc39f5dc097f200b53fb78aec16b34b9654465
lindo-semetsi-hub <lindosemetsi@gmail.com>
2025-09-15 11:41:28 +0200

    commit changes

--- src/callbackversion.ts
+++ src/callbackversion.ts
@@ -20,3 +20,24 @@ function fetchJSON(url: string, callback: (err: any, data?: any) => void) {
         callback(err);
         });
     }
+
+    // fetch weather
+    const weatherURL = "https://api.open-meteo.com/v1/forecast?latitude=40.71&longitude=-74.01&current_weather=true";
+
+    fetchJSON(weatherURL, (err, weatherData) => {
+    if (err) {
+        console.error ("Weather fetch error:", err);
+        return;
+    }
+    console.log("Weather data:", weatherData.current_weather);
+    const newsURL = "https://dummyjson.com/posts?limit=5";
+    
+    //fetch news
+    fetchJSON(newsURL, ( newsData) => {
+        if (err) {
+            console.error("News fetch error:", err);
+            return;
+        }
+        console.log("Latest fetch error:", newsData.posts);
+    });
+    });
